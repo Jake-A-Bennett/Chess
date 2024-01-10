90cc1ebef08b91006b853c3b4b0c0dc092641104
Jake <jakeabennettspokane@gmail.com>
2024-01-10 14:40:11 -0700

    Board Redesign in as Object

--- main.py
+++ main.py
@@ -6,56 +6,45 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+#light
+LIGHT = (232, 235, 239)
+#dark
+DARK = (125, 135, 150)
 coordinates = list()
 rectangles = dict()
-def create_board(rows, columns, size, posx, posy, spacing):
-    for column in range(columns):
-        for row in range(rows):
-            coordinates.append(((row+posx)*spacing, (column + posy)*spacing, size, size))
 
-def create_rectangles():
-    for coordinate in range(len(coordinates)):
-        rectangles[f"rect{coordinate}"] = pygame.Rect(coordinates[coordinate])
+class Board():
+    def __init__(self, rows, columns, size, pos_x, pos_y, colors):
+        self.board = [[0 for column in range(columns)] for row in range(rows)]
+        self.size = size
+        self.pos_x = pos_x
+        self.pos_y = pos_y
 
-#This is ugly but it works for now
-def draw_board(colors):
-    color = colors[0]
-    count = 0
-    switch = 0
-    num_switch = 0
-    for rectangle in rectangles.keys():
-        if count == 0:
-            color = colors[0]
-            count += 1
-        else:
-            color = colors[1]
-            count -= 1
-        switch += 1
-        if switch == 8:
-            if num_switch == 0:
-                count += 1
-                switch = 0
-                num_switch = 1
-            else:
-                count -= 1
-                switch = 0
-                num_switch = 0
+        self.create_board(colors)
 
+    def create_board(self, colors):
+        color = colors
+        for row in range(len(self.board)):
+            for column in range(len(self.board[row])):
+                if row % 2 == 0:
+                    if column % 2 == 0:
+                        color = colors[0]
+                    else:
+                        color = colors[1]
+                else:
+                    if column % 2 == 0:
+                        color = colors[1]
+                    else:
+                        color = colors[0]
 
-        pygame.draw.rect(surface=screen, color=color, rect=rectangles[rectangle])
+                coordinate = ((row+self.pos_x)*self.size, (column + self.pos_y)*self.size, self.size, self.size)
+                self.board[row][column] = [coordinate, color]
+    def draw_board(self, surface):
+        for row in range(len(self.board)):
+            for column in range(len(self.board)):
+                pygame.draw.rect(surface=surface, color=self.board[row][column][1], rect=self.board[row][column][0])
 
-
-
-
-
-
-create_board(rows=8, columns=8, size=60, posx=6, posy=1, spacing=65)
-create_rectangles()
-for rectangle in rectangles.keys():
-    print(rectangles[rectangle])
+board = Board(rows=8, columns=8, size=60, pos_x=6, pos_y=1, colors=[LIGHT, DARK])
 
 while running:
     for event in pygame.event.get():
@@ -65,7 +54,7 @@ while running:
     screen.fill("black")
 
     #draws all rectangle objects to the screens
-    draw_board([RED, BLUE])
+    board.draw_board(screen)
 
     pygame.display.flip()
 
