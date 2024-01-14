f801070267c9643e5c58eebe871dce886f366f54
Jake <jakeabennettspokane@gmail.com>
2024-01-14 14:33:06 -0700

    Full Board and Pieces

--- Pieces.py
+++ Pieces.py
@@ -1,79 +1,77 @@
 import pygame
 import os
 
-#doesn't work when being drawed on top of the board class which it needs to
-
-
-class Pawn:
+class Piece:
     def __init__(self, folder, file, x, y):
         self.img = pygame.image.load(os.path.join(folder, file))
         self.cur_x = x
         self.cur_y = y
 
-class Rook:
+class Pawn(Piece):
     def __init__(self, folder, file, x, y):
-        self.img = pygame.image.load(os.path.join(folder, file))
-        self.cur_x = x
-        self.cur_y = y
-
-class Bishop:
+        super().__init__(folder, file, x, y)
+class Rook(Piece):
     def __init__(self, folder, file, x, y):
-        self.img = pygame.image.load(os.path.join(folder, file))
-        self.cur_x = x
-        self.cur_y = y
+        super().__init__(folder, file, x, y)
 
-class Knight:
+class Bishop(Piece):
+        def __init__(self, folder, file, x, y):
+            super().__init__(folder, file, x, y)
+
+class Knight(Piece):
     def __init__(self, folder, file, x, y):
-        self.img = pygame.image.load(os.path.join(folder, file))
-        self.cur_x = x
-        self.cur_y = y
+        super().__init__(folder, file, x, y)
 
-class King:
+class King(Piece):
     def __init__(self, folder, file, x, y):
-        self.img = pygame.image.load(os.path.join(folder, file))
-        self.cur_x = x
-        self.cur_y = y
+        super().__init__(folder, file, x, y)
 
-class Queen:
+class Queen(Piece):
     def __init__(self, folder, file, x, y):
-        self.img = pygame.image.load(os.path.join(folder, file))
-        self.cur_x = x
-        self.cur_y = y
+        super().__init__(folder, file, x, y)
 
 
-class Pieces():
-    def __init__(self, board):
+class Pieces:
+    def __init__(self, board, pawn_row, piece_row, piece_images):
         self.board = board.board
-        self.pieces = {"Rook1": Rook(folder="Assets", file="Chess_rdt60.png", x=10, y=0),
-                       "Rook2": Rook(folder="Assets", file="Chess_rdt60.png", x=20, y=0),
-                       "Knight1": Knight(folder="Assets", file="Chess_ndt60.png", x=30, y=0),
-                       "Knight2": Knight(folder="Assets", file="Chess_ndt60.png", x=40, y=0),
-                       "Bishop1": Bishop(folder="Assets", file="Chess_bdt60.png", x=50, y=0),
-                       "Bishop2": Bishop(folder="Assets", file="Chess_bdt60.png", x=60, y=0),
-                       "King": King(folder="Assets", file="Chess_kdt60.png", x=70, y=0),
-                       "Queen": Queen(folder="Assets", file="Chess_qdt60.png", x=80, y=0)}
+        self.piece_images = piece_images
+        #The x and y values are not correct
+        self.pieces = {"Rook1": Rook(folder="Assets", file=piece_images["Rook"], x=0, y=0),
+                       "Knight1": Knight(folder="Assets", file=piece_images["Knight"], x=0, y=0),
+                       "Bishop1": Bishop(folder="Assets", file=piece_images["Bishop"], x=0, y=0),
+                       "Queen": Queen(folder="Assets", file=piece_images["Queen"], x=0, y=0),
+                       "King": King(folder="Assets", file=piece_images["King"], x=0, y=0),
+                       "Bishop2": Bishop(folder="Assets", file=piece_images["Bishop"], x=0, y=0),
+                       "Knight2": Knight(folder="Assets", file=piece_images["Knight"], x=0, y=0),
+                       "Rook2": Rook(folder="Assets", file=piece_images["Rook"], x=0, y=0)}
+
+        self.create_other_pieces(piece_row)
+        self.create_pawns(pawn_row)
 
 
 
-    def create_pawns(self):
+    def create_pawns(self, row):
         for pawn in range(8):
-            self.pieces[f"Pawn{pawn}"] = Pawn(folder="Assets", file="Chess_pdt60.png", x=pawn, y=pawn)
+            self.pieces[f"Pawn{pawn}"] = Pawn(folder="Assets", file=self.piece_images["Pawn"], x=self.board[pawn][row][0].left, y=self.board[pawn][row][0].top)
+
+    def create_other_pieces(self, row):
+        count = 0
+        for piece in self.pieces.values():
+            piece.cur_x = self.board[count][row][0].left
+            piece.cur_y = self.board[count][row][0].top
+            count += 1
 
     def draw_pieces(self, surface):
         for row in range(len(self.board)):
-            for column in range(len(self.board)-2, len(self.board[row])-1):
+            for column in range(0, len(self.board[row])):
                 square = self.board[row][column]
+                x = self.pieces[f"Pawn{column}"].cur_x
+                y = self.pieces[f"Pawn{column}"].cur_y
+                surface.blit(source=self.pieces[f"Pawn{column}"].img, dest=(x, y))
 
-                surface.blit(source=self.pieces[f"Pawn{column}"].img, dest=square[0])
 
-        surface.blit(source=self.pieces["Rook1"].img, dest=self.board[0][7][0])
-        surface.blit(source=self.pieces["Rook2"].img, dest=self.board[7][7][0])
-        surface.blit(source=self.pieces["Knight1"].img, dest=self.board[1][7][0])
-        surface.blit(source=self.pieces["Knight2"].img, dest=self.board[6][7][0])
-        surface.blit(source=self.pieces["Bishop1"].img, dest=self.board[2][7][0])
-        surface.blit(source=self.pieces["Bishop2"].img, dest=self.board[5][7][0])
-        surface.blit(source=self.pieces["Queen"].img, dest=self.board[3][7][0])
-        surface.blit(source=self.pieces["King"].img, dest=self.board[4][7][0])
+        for piece in self.pieces.values():
+            surface.blit(source=piece.img, dest=(piece.cur_x, piece.cur_y))
 
 
 
--- main.py
+++ main.py
@@ -14,19 +14,65 @@ LIGHT = (232, 235, 239)
 #dark
 DARK = (125, 135, 150)
 
+piece_images_white = {"Rook": "Chess_rlt60.png",
+                "Bishop": "Chess_blt60.png",
+                "Knight": "Chess_nlt60.png",
+                "Queen": "Chess_qlt60.png",
+                "King": "Chess_klt60.png",
+                "Pawn": "Chess_plt60 (1).png"}
+
+piece_images_black = {"Rook": "Chess_rdt60.png",
+                "Bishop": "Chess_bdt60.png",
+                "Knight": "Chess_ndt60.png",
+                "Queen": "Chess_qdt60.png",
+                "King": "Chess_kdt60.png",
+                "Pawn": "Chess_pdt60.png"}
+
+
 
 board = Board(rows=8, columns=8, size=60, pos_x=6, pos_y=1, colors=[LIGHT, DARK])
-pieces = Pieces(board=board)
-pieces.create_pawns()
+pieces = Pieces(board=board, pawn_row=6, piece_row=7, piece_images=piece_images_white)
+black_pieces = Pieces(board=board, pawn_row=1, piece_row=0, piece_images=piece_images_black)
+
+def touched():
+    for piece in pieces.pieces.values():
+        rect = piece.img.get_rect(x=piece.cur_x, y=piece.cur_y)
+        if rect.collidepoint(pygame.mouse.get_pos()):
+            return piece
+
+    for piece in black_pieces.pieces.values():
+        rect = piece.img.get_rect(x=piece.cur_x, y=piece.cur_y)
+        if rect.collidepoint(pygame.mouse.get_pos()):
+            return piece
+
+
+
+def touched_rect():
+    for row in board.board:
+        for column in row:
+            if column[0].collidepoint(pygame.mouse.get_pos()):
+                return column[0]
+
 while running:
+
+    buttons = pygame.mouse.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif buttons[0] and touched() is not None:
+            touched().cur_x = pygame.mouse.get_pos()[0] - 25
+            touched().cur_y = pygame.mouse.get_pos()[1] - 25
+        elif not buttons[0] and touched() is not None:
+            touched().cur_x = touched_rect().left
+            touched().cur_y = touched_rect().top
+
+
 
     screen.fill("black")
     #draws all rectangle objects to the screens
     board.draw_board(screen)
     pieces.draw_pieces(screen)
+    black_pieces.draw_pieces(screen)
 
     pygame.display.update()
 
