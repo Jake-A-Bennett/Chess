f2b32cb426a40de3389e2eaac4cf8267cd6017a9
Jake <jakeabennettspokane@gmail.com>
2024-01-14 16:51:08 -0700

    Grab pieces and return to previous position on error

---
+++ Settings.py
@@ -0,0 +1,24 @@
+WHITE_PIECES_IMG = {"Rook": "Chess_rlt60.png",
+                "Bishop": "Chess_blt60.png",
+                "Knight": "Chess_nlt60.png",
+                "Queen": "Chess_qlt60.png",
+                "King": "Chess_klt60.png",
+                "Pawn": "Chess_plt60 (1).png"}
+
+BLACK_PIECES_IMG = {"Rook": "Chess_rdt60.png",
+                "Bishop": "Chess_bdt60.png",
+                "Knight": "Chess_ndt60.png",
+                "Queen": "Chess_qdt60.png",
+                "King": "Chess_kdt60.png",
+                "Pawn": "Chess_pdt60.png"}
+
+BOARD_ROWS = 8
+BOARD_COLUMNS = 8
+BOARD_SIZE = 60
+BOARD_POS_X = 6
+BOARD_POS_Y = 1
+
+COLORS = {"LIGHT": (232, 235, 239),
+          "DARK": (125, 135, 150)}
+
+FILL = "black"
